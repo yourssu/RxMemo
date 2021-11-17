46529052a19fe4c51321af5d7a9d3b301fb716e4
Yonghyun Kim <lygon55555@gmail.com>
2021-11-17 20:16:17 +0900

    implement MemoDetailViewModel

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -27,7 +27,28 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
 
     func bindViewModel() {
+        // navigationTitle 바인딩
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        // tableView 바인딩
+        viewModel.contents
+            .bind(to: listTableView.rx.items) { tableView, row, value in
+                switch row {
+                case 0: // 첫 번째 셀이라면 contentCell을 꺼내서 label에 문자열을 표시하고 리턴
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                case 1: // 두 번째 셀이라면 dateCell을 꺼내서 같은 방식으로 구현
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                default:
+                    fatalError()
+                }
+            }
+            .disposed(by: rx.disposeBag)
     }
 
 }
--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -8,7 +8,48 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    
+    // 이전 Scene에서 전달된 메모 저장
+    let memo: Memo
+    
+    
+    // 날짜를 문자열로 바꿀 때 사용
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+    
+    
+    // 메모 내용은 tableView에 표시됨
+    // 첫 번째 셀에는 메모 내용이 표시되고
+    // 두 번째 셀에는 날짜가 표시됨
+    // 메모 목록에서 구현한 것처럼 tableView에 데이터를 표시할 때는 Observable과 바인딩함
+    // tableView에 표시할 데이터는 문자열 2개임
+    // 그래서 문자열 배열을 방출함
+    // 그냥 Observable로 해도 되는데 BehaviorSubject로 한 이유는?
+    // 메모를 편집한 다음에 다시 보기 화면으로 오면 편집한 내용이 반영돼야 함
+    // 이렇게 하기 위해서는 새로운 문자열 배열을 방출해야 함
+    // 일반 Observable로 선언하면 이게 불가능
+    var contents: BehaviorSubject<[String]>
+    
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+        contents = BehaviorSubject<[String]>(value: [
+                                                memo.content,
+                                                formatter.string(from: memo.insertDate)
+        ])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
+    
+    
+    
     
 }
